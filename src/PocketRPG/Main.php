e1a9754559da0bc3dacfc0c56bad7acc29982fa5
Blabla <st.ten.veldhuis@gmail.com>
2016-06-05 18:04:10 +0200

    Update Main.php

--- src/PocketRPG/Main.php
+++ src/PocketRPG/Main.php
@@ -3,7 +3,30 @@
 namespace PocketRPG;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\command\Command;
+use pocketmine\command\CommandExecutor;
+use pocketmine\command\PluginCommand;
+use pocketmine\utils\TextFormat as TF;
+use pocketmine\utils\Config;
+use pocketmine\permission\Permission;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\math\Vector3;
+use pocketmine\level\Level;
+use pocketmine\level\Position;
 
 class Main extends PluginBase implements Listener {
 
+  public function onEnable() {
+    $this->getLogger()->info(TF:: GREEN . "Enabling PocketRPG");
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    @mkdir($this->getDataFolder());
+    $this->saveResource("config.yml");
+    $this->config = new Config($this->getDataFolder(). "config.yml", Config::YAML);
+  }
+  
+  public function onDisable() {
+    $this->getLogger()->info(TF:: RED . "Disabling PocketRPG");
+  }
 }
