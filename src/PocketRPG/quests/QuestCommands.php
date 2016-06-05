7375cd3ba5759414f70f5bae7fe70586a85a6184
Blabla <st.ten.veldhuis@gmail.com>
2016-06-05 18:08:37 +0200

    Create QuestCommands.php

---
+++ src/PocketRPG/quests/QuestCommands.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace PocketRPG\quests;
+
+use PocketRPG\Main;
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\utils\TextFormat as TF;
+use pocketmine\utils\Config;
+use pocketmine\permission\Permission;
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmine\level\Level;
+
+class QuestCommands extends Main {
+
+}
