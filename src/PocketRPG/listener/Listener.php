018c3d2d409326bdec59f4f956337ba4ca98412b
Blabla <st.ten.veldhuis@gmail.com>
2016-06-06 10:19:14 +0200

    Update Listener.php

--- src/PocketRPG/listener/Listener.php
+++ src/PocketRPG/listener/Listener.php
@@ -3,6 +3,8 @@
 namespace PocketRPG\Listener;
 
 use PocketRPG\Main;
+use pocketmine\utils\TextFormat as TF;
+use pocketmine\utils\Config;
 use pocketmine\event\Listener;
 use pocketmine\event\Event;
 use pocketmine\level\Level;
@@ -15,6 +17,7 @@ use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\event\player\PlayerItemHeldEvent;
 use pocketmine\level\particle\CriticalParticle;
+use pocketmine\event\player\PlayerItemHeldEvent;
 
 class Listener extends Main implements Listener {
   
@@ -25,21 +28,58 @@ class Listener extends Main implements Listener {
         $cfglevel = $this->config->get("RPG_LEVEL");
         if($damager->getLevelByName() == $cfglevel) {
             if($p->hasPermission("class.assassin")) {
-                if($damager->getItemInHand()->getId() == 388) {
+              if($damager->getItemInHand()->getId() == 388) {
                 $x = $hit->x;
                 $y = $hit->y;
                 $z = $hit->z;
                 $hitpos = $hit->getPosition(new Vector3($x, $y, $z));
                 $level->addParticle(new CriticalParticle($hitpos));
                 $this->setDamage(getDamage() + 3);
-                }
+              }
             } elseif($p->hasPermission("class.mage")) {
-                if($damage->getItemInHand()->getId() == 280) {
-                  
-                }
+              if($damage->getItemInHand()->getId() == 280) {
+                $x = $hit-r>x;
+                $y = $hit->y;
+                $z = $hit->z;
+                $hitpos = $hit->getPosition(new Vector3($x, $y, $z));
+                $level->addParticle(new LavaParticle($hitpos));
+                $this->setKnockBack(1);
+                $hit->setOnFire(4);
+                $this->setDamage(getDamage() + 4)
+              }
+            } elseif($p->hasPermission("class.tanker")) {
+              if($damager->getItemInHand()->getId() == 328) {
+                $x = $hit->x;
+                $y = $hit->y;
+                $z = $hit->z;
+                $hitpos = $hit->getPosition(new Vector3($x, $y, $z));
+                $level->addParticle(new ExplodeParticle($hitpos));
+                $this->setKnockBack(3);
+                $this->setDamage(getDamage() + 2);
+              }
+            } elseif($p->hasPermission("class.warrior")) {
+              if($damager->getItemInHand()->getId() == 267) {
+                $x = $hit->x;
+                $y = $hit->y;
+                $z = $hit->z;
+                $hitpos = $hit->getPosition(new Vector3($x, $y, $z));
+                $level->addParticle(new CritialParticle($hitpos));
+                $this->setKnockBack(2);
+                $this->setDamage(getDamage() + 6);
+              }
             }
         }
     }
   }
-  
+  public function onItemHeld(PlayerItemHeldEvent $event) {
+    $p = $event->getPlayer();
+    $cfglevel = $this->config->get("RPG_LEVEL");
+    if($p->getLevelByName == $cfglevel) {
+      if($p->getItemInHand() == 347) {
+        if($p->hasPermission("class.assassin")) {
+        $effect = Effect::getEffect(14)->setDuration(1)->setAmplifier(1)->setVisible(true);
+        $p->addEffect($effect);
+      }
+    }
+  }
 }
