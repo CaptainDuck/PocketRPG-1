94a9eb4aebf3db37bf97ba4c6a3fd3505c0bc999
Blabla <st.ten.veldhuis@gmail.com>
2016-06-06 08:51:27 +0200

    Update Listener.php

--- src/PocketRPG/listener/Listener.php
+++ src/PocketRPG/listener/Listener.php
@@ -19,6 +19,27 @@ use pocketmine\level\particle\CriticalParticle;
 class Listener extends Main implements Listener {
   
   public function onFight(EntityDamageEvent $event) {
-  
+    if($event instance EntityDamageByEntityEvent && $event->getDamager() instanceof Player) {
+        $hit = $event->getEntity();
+        $damager = $event->getDamager();
+        $cfglevel = $this->config->get("RPG_LEVEL");
+        if($damager->getLevelByName() == $cfglevel) {
+            if($p->hasPermission("class.assassin")) {
+                if($damager->getItemInHand()->getId() == 388) {
+                $x = $hit->x;
+                $y = $hit->y;
+                $z = $hit->z;
+                $hitpos = $hit->getPosition(new Vector3($x, $y, $z));
+                $level->addParticle(new CriticalParticle($hitpos));
+                $this->setDamage(getDamage() + 3);
+                }
+            } elseif($p->hasPermission("class.mage")) {
+                if($damage->getItemInHand()->getId() == 280) {
+                  
+                }
+            }
+        }
+    }
   }
+  
 }
