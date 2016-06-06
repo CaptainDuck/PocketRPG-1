ee28e604d630ac7aa7f48ca351bbd220126a5782
Blabla <st.ten.veldhuis@gmail.com>
2016-06-06 15:19:45 +0200

    Create RPGcommands.php

---
+++ src/PocketRPG/commands/RPGcommands.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace PocketRPG\commands;
+
+use PocketRPG\Main;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\utils\TextFormat as TF;
+use pocketmine\utils\Config;
+
+class RPGcommands extends Main {
+
+}
