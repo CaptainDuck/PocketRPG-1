dbb063a398176907a825123fcec720b67f9e3e34
Blabla <st.ten.veldhuis@gmail.com>
2016-06-09 13:38:04 +0200

    Update Main.php

--- src/PocketRPG/Main.php
+++ src/PocketRPG/Main.php
@@ -33,7 +33,6 @@ class Main extends PluginBase implements Listener {
     @mkdir($this->getDataFolder());
     $this->saveResource("party.yml");
     $this->party = new Config($this->getDataFolder(). "party.yml", Config::YAML);
-    }
   }
   
   public function onDisable() {
